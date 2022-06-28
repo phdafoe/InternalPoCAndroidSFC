6e164dc34e98ac809eac0a7146d9292387d56af8
Phdafoe <phdafoe@gmail.com>
2022-06-28 12:23:01 +0200

    [WIP] - PoC SFC

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        webView.setWebChromeClient(object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    request.grant(request.resources)
+                }
+            }
+        })
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("https://bcmspassdigitalcua.global.npsa.carrefour.es/?origen=MIC4")
+    }
+}
